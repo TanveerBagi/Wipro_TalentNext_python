bd2b2fd57ca2f240bac06bbc7bd931e41c7abd0b
Tanveer Singh Bagi <152624343+TanveerBagi@users.noreply.github.com>
2025-09-04 23:00:29 +0530

    Update Regression.py

--- Regression_and_Classification/Regression.py
+++ Regression_and_Classification/Regression.py
@@ -31,3 +31,34 @@ r2 = r2_score(y_test, y_pred)
 print("MSE:", mse)
 print("RMSE:", rmse)
 print("R²:", r2)
+
+#Q2. Create a model that can predict the profit based on its features. Use appropriate evaluation metrics. Dataset: 50_Startups.csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+df = pd.read_csv("50_Startups.csv")
+print(df.head())
+
+df = pd.get_dummies(df, drop_first=True)
+
+X = df.drop("Profit", axis=1)
+y = df["Profit"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R²:", r2)
